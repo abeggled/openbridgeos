b3137c90b3f621880c94af0393cb58793b7ce330
Daniel <abeggled@users.noreply.github.com>
2026-06-04 13:31:24 +0200

    Print Raspberry Pi image compatibility metadata

--- scripts/images/print-image-build-plan.sh
+++ scripts/images/print-image-build-plan.sh
@@ -20,6 +20,10 @@ OBOS_OUTPUT_FORMAT=
 OBOS_BASE_PACKAGES=
 OBOS_PROVISION_SCRIPT=
 OBOS_FIRST_BOOT_SERVICE=
+OBOS_RPI_NETWORK_INSTALLER_COMPATIBLE=
+OBOS_RPI_BOOT_MEDIA=
+OBOS_RPI_FIRMWARE_MODE=
+OBOS_KERNEL_REQUIRED_CONFIG=
 OBOS_NOTES=
 
 # shellcheck disable=SC1090
@@ -35,6 +39,10 @@ output_format=${OBOS_OUTPUT_FORMAT}
 base_packages=${OBOS_BASE_PACKAGES}
 provision_script=${OBOS_PROVISION_SCRIPT}
 first_boot_service=${OBOS_FIRST_BOOT_SERVICE}
+rpi_network_installer_compatible=${OBOS_RPI_NETWORK_INSTALLER_COMPATIBLE:-false}
+rpi_boot_media=${OBOS_RPI_BOOT_MEDIA:-n/a}
+rpi_firmware_mode=${OBOS_RPI_FIRMWARE_MODE:-n/a}
+kernel_required_config=${OBOS_KERNEL_REQUIRED_CONFIG:-n/a}
 notes=${OBOS_NOTES}
 
 build_contract:
@@ -46,3 +54,12 @@ build_contract:
   - enable ${OBOS_FIRST_BOOT_SERVICE} for target appliance initialization
   - emit ${OBOS_OUTPUT_FORMAT} artifact plus checksum
 EOF
+
+if [ "${OBOS_IMAGE_KIND}" = "rpi-image" ]; then
+  cat <<EOF
+rpi_contract:
+  - image must be Raspberry Pi Network Installer compatible
+  - image must boot from all declared media: ${OBOS_RPI_BOOT_MEDIA}
+  - kernel config must include: ${OBOS_KERNEL_REQUIRED_CONFIG}
+EOF
+fi
